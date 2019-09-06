a55e73739be4d6778f72793a1eeccefe52f6a66d
omen <omen@10.11.144.20>
2019-09-05 20:58:28 -0500

    second commind

--- capstoneproject/src/main/java/com/pw/appconsole/App.java
+++ capstoneproject/src/main/java/com/pw/appconsole/App.java
@@ -6,7 +6,7 @@ package com.pw.appconsole;
 public class App {
 
 	public static void main(String[] args)  {
-		
+		//:v;
 	}
 
 }
---
+++ capstoneproject/src/main/java/com/pw/business/Class1.java
@@ -0,0 +1,5 @@
+package com.pw.business;
+
+public class Class1 {
+
+}
