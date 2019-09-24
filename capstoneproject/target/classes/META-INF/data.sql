6f52444bb4e01e15ba2821260fc3f25849b9d08b
Usuario <Usuario@10.11.143.74>
2019-09-24 15:52:29 -0500

    Todo Almacén

--- capstoneproject/target/classes/META-INF/data.sql
+++ capstoneproject/target/classes/META-INF/data.sql
@@ -6,3 +6,32 @@ INSERT INTO proveedor(nproveedor,numtelefono_proveedor,tdireccion_proveedor ) VA
 INSERT INTO proveedor(nproveedor,numtelefono_proveedor,tdireccion_proveedor ) VALUES ('Proveedor5',  984531723, 'av Industrial 128');
 INSERT INTO proveedor(nproveedor,numtelefono_proveedor,tdireccion_proveedor ) VALUES ('Proveedor6',  984531123, 'av Industrial 153');
 INSERT INTO proveedor(nproveedor,numtelefono_proveedor,tdireccion_proveedor ) VALUES ('Proveedor7', 984531133, 'av Industrial 157');
+
+INSERT INTO marcas(nombre_marca) VALUES ('Coca Cola');
+INSERT INTO marcas(nombre_marca) VALUES ('Inka Kola');
+INSERT INTO marcas(nombre_marca) VALUES ('Kola Real');
+INSERT INTO marcas(nombre_marca) VALUES ('Pepsi');
+INSERT INTO marcas(nombre_marca) VALUES ('Sprite');
+
+INSERT INTO sabores(nombre_sabor) VALUES ('Naranja');
+INSERT INTO sabores(nombre_sabor) VALUES ('Limon');
+INSERT INTO sabores(nombre_sabor) VALUES ('Cola Amarilla');
+INSERT INTO sabores(nombre_sabor) VALUES ('Cola');
+
+INSERT INTO unidades(nombre_unidad) VALUES ('Litro');
+INSERT INTO unidades(nombre_unidad) VALUES ('Mililitro');
+INSERT INTO unidades(nombre_unidad) VALUES ('Gramo');
+INSERT INTO unidades(nombre_unidad) VALUES ('Kilogramo');
+
+INSERT INTO cargos(nombre_cargo) VALUES ('Cocinero');
+INSERT INTO cargos(nombre_cargo) VALUES ('Motorizado');
+INSERT INTO cargos(nombre_cargo) VALUES ('Recepcionista de llamadas');
+INSERT INTO cargos(nombre_cargo) VALUES ('Mozo');
+
+INSERT INTO tipos_pedidos(nombre_tipos) VALUES ('Comer aquí');
+INSERT INTO tipos_pedidos(nombre_tipos) VALUES ('Para llevar');
+INSERT INTO tipos_pedidos(nombre_tipos) VALUES ('Delivery');
+
+INSERT INTO sucursales(numero_telefono, direccion) VALUES (988574951, 'Enrique Segoviano, Santa Anita');
+INSERT INTO sucursales(numero_telefono, direccion) VALUES (980975451, 'Calle Nueva, Vitarte');
+
