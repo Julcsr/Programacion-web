896a7c43c88c8bdd9a026932ab3d35625e3b3ae6
omen <omen@192.168.1.62>
2019-11-18 08:48:43 -0500

    Correccion mensaje y vistas

--- capstoneproject-01/src/main/java/com/hampcode/articlesapp/service/OfficeService.java
+++
@@ -1,9 +0,0 @@
-package com.hampcode.articlesapp.service;
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import com.hampcode.articlesapp.model.Office;
-
-public interface OfficeService extends CrudService<Office, Long>{
-	Page<Office> findAll(Pageable pageable);
-}
--- capstoneproject-01/src/main/java/com/hampcode/articlesapp/service/PositionService.java
+++
@@ -1,9 +0,0 @@
-package com.hampcode.articlesapp.service;
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import com.hampcode.articlesapp.model.Position;
-
-public interface PositionService extends CrudService<Position, Long> {
-	Page<Position> findAll(Pageable pageable);
-}
